367b2c37595b859ab9518e6f9f80a9d2487ce2ec
Ajay Kumar Pandey <ajaypandey8322@gmail.com>
2024-07-28 17:56:34 +0530

    added insert and delete

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,5 +1,11 @@
 package com.ecommerce.project.service;
 
-public class CategoryService {
+import com.ecommerce.project.model.Category;
 
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+    String deleteCategory(Long categoryId);
 }
